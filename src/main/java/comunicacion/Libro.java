208ab16be70488bdf2e6395f30490baac74aaf00
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2021-12-03 14:50:05 -0500

    Update Libro.java

--- src/main/java/comunicacion/Libro.java
+++ src/main/java/comunicacion/Libro.java
@@ -50,7 +50,7 @@ public class Libro extends Escrito {
 	@Override
 	public
 	int palabrasTotales(int palabrasPagina) {
-		return getPaginas()*2;
+		return getPaginas()*2*palabrasPagina;
 	}
 
 	@Override
